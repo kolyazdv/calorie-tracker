d6515b875bc53f6341143cd108efb1c7f02a1e0c
Nikolay Zadvorniy <kolyazdv@MacBook-Pro-Nikolay.local>
2025-05-06 15:08:30 +0200

    Добавлен дашборд и улучшена структура главной страницы

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   devise_for :users
-  root to: "profiles#show"  # Показываем профиль сразу после входа
+
+  get "home/index"
+  get "dashboard", to: "dashboard#index", as: :dashboard
+  root "home#index"
 
   resource :profile, only: [ :show ]
 end
